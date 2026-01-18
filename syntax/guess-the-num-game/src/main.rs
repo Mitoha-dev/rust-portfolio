3d37351a553e796e730d161c6f5fbe22d905afef
mitoha-dev <mitoha.dev@outlook.jp>
2026-01-18 23:09:13 +0900

    feat:Complete the number guessing game

--- syntax/guess-the-num-game/src/main.rs
+++ syntax/guess-the-num-game/src/main.rs
@@ -1,26 +1,35 @@
 use std::io; //Use standard library for input and output
 use rand::Rng; //Random number generation
+use std::cmp::Ordering; //Comparison trait
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-    println!("Please input your guess.");
+        let mut guess = String::new(); //create empty string
 
-    let mut guess = String::new(); //create empty string
+        io::stdin() //get standard input hundle
+            .read_line(&mut guess) //subject omission (Method Chain). "&" denotes a reference
+            .expect("Failed to read line");
 
-    io::stdin() //get standard input hundle
-        .read_line(&mut guess) //subject omission (Method Chain). "&" denotes a reference
-        .expect("Failed to read line");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        //.trim() removes whitespace/newline characters like "\n"," ".
+        //.parse() converts string to number
 
-    match secret_number.cmp(&50) {//.cmp() returns "std::cmp::Ordering::Less, Greater, or Equal".
-        std::cmp::Ordering::Less => println!("Too small!"),
-        std::cmp::Ordering::Greater => println!("Too big!"),
-        std::cmp::Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {//.cmp() returns "std::cmp::Ordering::Less, Greater, or Equal".
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+
+        println!("You guessed: {}", guess);
     }
-
-    println!("You guessed: {}", guess);
 }
