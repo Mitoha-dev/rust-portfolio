fb580d06ce5936934a7179d6d496788fea1721f7
u10-dev <e1302025064@oit.ac.jp>
2026-04-15 12:19:21 +0900

    fix:linear-search-02

--- algorithm/linear-search/examples/linear-search-02.rs
+++ algorithm/linear-search/examples/linear-search-02.rs
@@ -1,25 +1,27 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-
 fn main() {
-
     const N: usize = 20;
-    const TARGET = 4;
+    const TARGET: usize = 4;
 
     let v = random_array(N);
-    let mut ans = 0;
+    let mut ans = None;
 
-    for i in &v{
-        if v[*i] == TARGET {
-            ans = *i;
+    for (index, &value) in v.iter().enumerate() {
+        if value == TARGET {
+            ans = Some(index);
+            break;
         }
     }
-    println!("index is {}", ans+1);
-    println!("{:?}", v);
+
+    println!("array = {:?}", v);
+
+    match ans {
+        Some(i) => println!("index is {}", i),
+        None => println!("not found"),
+    }
 }
-
-
 
 fn random_array(n: usize) -> Vec<usize> {
     let mut arr: Vec<usize> = (0..n).collect();
